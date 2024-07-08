a542ef07d6cb8cb41618d2f7c62bb9ae4adca42b
JDMCM <jmcmahon@trinity.edu>
2024-07-07 21:17:14 -0500

    Added user inputs to choose pairs

--- eventScript/script.py
+++ eventScript/script.py
@@ -28,13 +28,40 @@ df = df.withColumn('part1Index', split.getItem(1).cast(DoubleType())) \
     .withColumn('time', split.getItem(3).cast(DoubleType())).drop('value')
 dfc = df.select('index','time').withColumnRenamed('index','Cumulative Event Count')
 
+
+
+dfup = df.sort('time').dropDuplicates(['part1Index','part2Index'])
+dfdwn = df.sort('time',ascending=False).dropDuplicates(['part1Index','part2Index']).withColumnRenamed('time','time1')
+dfdiff = dfup.drop('index').join(dfdwn.drop('index'), on=['part1Index','part2Index'])
+dfdiff = dfdiff.withColumn('collison length',col('time1')-col('time')).drop('time','time1')
+#dfdiff.show()
+
+#display the particle pairs with the most events and the time over all the events
+dfwuh = df.groupBy('part1Index','part2Index').count().sort('count',ascending=False). \
+    join(dfdiff,on=['part1Index','part2Index']).sort('count',ascending=False)
+dfwuh.show()
+print('Select indexes for detla t between events')
+input2 = input('Enter first particle Index of a pair ').replace('"', '')
+input3 = input('Enter second particle Index of a pair ').replace('"', '')
+#get the time between events for particle pairs
+dfdelta = df.filter((col('part1Index') == input2) & (col('part2Index') == input3)).sort('time').drop('index','part1Index','part2Index'). \
+    withColumn('index', monotonically_increasing_id())
+dfta = dfdelta.filter(dfdelta.index > 0.0).withColumn('newdex', monotonically_increasing_id()).drop('index'). \
+    withColumnRenamed('time','time0')
+dfdel = dfdelta.filter(col('index') < dfdelta.count()-2).withColumn('newdex',monotonically_increasing_id()).drop('index'). \
+    withColumnRenamed('time','time1')
+dfdelta = dfta.join(dfdel, on=['newdex']) \
+    .withColumn('delta_t',col('time0')-col('time1')).drop('time0','time1') \
+    .withColumnRenamed('newdex','event_count')
+
 maxt = df.select(max('time')).collect()[0][0]
-bin_num = 1000
+bin_num = 5000
 dfo = df.withColumn('time-range', ceil(col('time')/(maxt/bin_num))*(maxt/bin_num)) \
     .select('time-range').groupBy('time-range').count().sort('time-range')
 
-dfuh = dfo.withColumn('index',monotonically_increasing_id()%(bin_num/100)/(bin_num/100))
-dfuh = dfuh.groupBy('index').sum('count').sort('index')
+#for checking within timestep event distribution
+# dfuh = dfo.withColumn('index',monotonically_increasing_id()%(bin_num/100)/(bin_num/500))
+# dfuh = dfuh.groupBy('index').sum('count').sort('index')
 #dfuh.show()
 
 
@@ -114,7 +141,11 @@ pevol.plot(ax=axes[0,1], kind = 'scatter', x = 'time',y = 'Unique Particle Colli
 pdfc = dfc.toPandas()
 pdfc.plot(ax=axes[0,0], kind='scatter', x='time',y='Cumulative Event Count', title='Cumulative Event Count vs. Time')
 
-pdfuh =dfuh.toPandas()
-pdfuh.plot(kind='scatter',x='index', y="sum(count)",xlabel='Fraction of Bigtimestep',ylabel='Event Count',title='Event counts sumed over fractions of bigtimestep')
+pdfdelta = dfdelta.toPandas()
+pdfdelta.plot(kind='scatter', x='event_count', y='delta_t')
+
+# pdfuh =dfuh.toPandas()
+# pdfuh.plot(kind='scatter',x='index', y="sum(count)",xlabel='Fraction of Bigtimestep',ylabel='Event Count',title='Event counts sumed over fractions of bigtimestep')
+
 plt.show()
 
